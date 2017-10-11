bbdd5a04e7a1bf0db925aa4350b4e8f0acc48481
diankun.qiu <diankun.qiu@ecovacs.com>
2017-10-11 17:25:01 +0800

    add practice 05,06

--- app/src/main/java/com/hencoder/hencoderpracticedraw6/practice/Practice05MultiProperties.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw6/practice/Practice05MultiProperties.java
@@ -9,10 +9,13 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import com.hencoder.hencoderpracticedraw6.R;
+import com.hencoder.hencoderpracticedraw6.Utils;
 
 public class Practice05MultiProperties extends ConstraintLayout {
     Button animateBt;
     ImageView imageView;
+
+    boolean animate;
 
     public Practice05MultiProperties(Context context) {
         super(context);
@@ -39,6 +42,23 @@ public class Practice05MultiProperties extends ConstraintLayout {
             @Override
             public void onClick(View v) {
                 // TODO 在这里处理点击事件，同时对多个属性做动画
+
+                if (!animate) {
+                    imageView.animate()
+                            .translationX(Utils.dpToPixel(200))
+                            .rotation(360)
+                            .scaleX(1)
+                            .scaleY(1)
+                            .alpha(1);
+                }else{
+                    imageView.animate()
+                            .translationX(0)
+                            .rotation(0)
+                            .scaleX(0)
+                            .scaleY(0)
+                            .alpha(0);
+                }
+                animate = !animate;
             }
         });
     }
